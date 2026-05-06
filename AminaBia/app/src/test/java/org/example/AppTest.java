623be0bb6792e612ccab2bf223c9fc1e532b49aa
LAPTA <Mina7study@gmail.com>
2026-05-06 17:54:01 +0100

    test: add precise discount calculation test

--- AminaBia/app/src/test/java/org/example/AppTest.java
+++ AminaBia/app/src/test/java/org/example/AppTest.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import org.junit.Test;
+
+
 import static org.junit.Assert.*;
 import java.util.Arrays;
 
@@ -42,5 +44,16 @@ public class AppTest {
         );
         // subtotal = 100, discount = 5, tax = 4.75, total = 99.75
         assertEquals(99.75, result, 0.01);
+    }
+    @Test
+    public void testSave20Discount() {
+        double result = app.calculate(
+            Arrays.asList(200.0),
+            Arrays.asList(1),
+            "REGULAR",
+            "SAVE20"
+        );
+        // subtotal = 200, discount = 40, tax = 16, total = 176
+        assertEquals(176.0, result, 0.01);
     }
 }
